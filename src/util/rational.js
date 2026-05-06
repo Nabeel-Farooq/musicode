ec2b9407289c0d6d7e5ff40f0bac0ae7a74b2148
Nabeel Farooq <45237521+Nabeel-Farooq@users.noreply.github.com>
2026-05-06 14:37:56 +0500

    Update rational.js

--- src/util/rational.js
+++ src/util/rational.js
@@ -1,222 +1,101 @@
-function Rational(integer = 0, numerator = 0, denominator = 1)
-{
-	this.integer = integer;
-	this.numerator = numerator;
-	this.denominator = denominator;
-	this.normalize();
-}
+class Rational {
+  constructor(numerator = 0, denominator = 1) {
+    if (denominator === 0) throw new Error("Denominator cannot be 0");
 
+    this.numerator = numerator;
+    this.denominator = denominator;
 
-Rational.prototype.clone = function()
-{
-	return new Rational(this.integer, this.numerator, this.denominator);
-}
+    this.normalize();
+  }
 
+  static fromMixed(integer, numerator, denominator) {
+    return new Rational(
+      integer * denominator + numerator,
+      denominator
+    );
+  }
 
-Rational.fromFloat = function(value, step)
-{
-	var rational = new Rational(Math.floor(value));
-	
-	while (rational.asFloat() < value)
-		rational.add(step);
-	
-	return rational;
-}
+  clone() {
+    return new Rational(this.numerator, this.denominator);
+  }
 
+  static gcd(a, b) {
+    while (b !== 0) {
+      [a, b] = [b, a % b];
+    }
+    return Math.abs(a);
+  }
 
-Rational.prototype.toString = function()
-{	
-	if (this.integer == 0 && this.numerator == 0)
-		return "0";
-	
-	var string = "";
-	if (this.integer != 0)
-		string += this.integer.toString();
-	
-	if (this.numerator != 0)
-	{
-		if (this.integer != 0)
-			string += "+";
-		
-		string += this.numerator.toString() + "/" + this.denominator.toString();
-	}
-	
-	return string;
-}
+  static lcm(a, b) {
+    return (a * b) / Rational.gcd(a, b);
+  }
 
+  normalize() {
+    if (this.denominator < 0) {
+      this.numerator *= -1;
+      this.denominator *= -1;
+    }
 
-Rational.prototype.normalize = function()
-{
-	while (this.numerator < 0)
-	{
-		this.numerator += this.denominator;
-		this.integer -= 1;
-	}
-	
-	while (this.numerator >= this.denominator)
-	{
-		this.numerator -= this.denominator;
-		this.integer += 1;
-	}
-	
-	this.trySimplifyBy(2);
-	this.trySimplifyBy(3);
-	this.trySimplifyBy(5);
-	this.trySimplifyBy(7);
-	this.trySimplifyBy(11);
-	this.trySimplifyBy(13);
-}
+    const gcd = Rational.gcd(this.numerator, this.denominator);
 
+    this.numerator /= gcd;
+    this.denominator /= gcd;
+  }
 
-Rational.prototype.trySimplifyBy = function(divider)
-{
-	while ((this.numerator % divider) == 0 && (this.denominator % divider) == 0)
-	{
-		this.numerator /= divider;
-		this.denominator /= divider;
-	}
-}
+  asFloat() {
+    return this.numerator / this.denominator;
+  }
 
+  toString() {
+    const integer = Math.trunc(this.numerator / this.denominator);
+    const remainder = Math.abs(this.numerator % this.denominator);
 
-Rational.prototype.asFloat = function()
-{
-	return this.integer + this.numerator / this.denominator;
-}
+    if (remainder === 0) return `${integer}`;
+    if (integer === 0) return `${this.numerator}/${this.denominator}`;
 
+    return `${integer}+${remainder}/${this.denominator}`;
+  }
 
-Rational.prototype.getAsNumerator = function(denominator)
-{
-	return (this.integer * denominator) + (this.numerator * (denominator / this.denominator));
-}
+  add(other) {
+    const lcm = Rational.lcm(this.denominator, other.denominator);
 
+    const n1 = this.numerator * (lcm / this.denominator);
+    const n2 = other.numerator * (lcm / other.denominator);
 
-Rational.prototype.negate = function()
-{
-	this.integer = -this.integer - 1;
-	this.numerator = this.denominator - this.numerator;
-	
-	this.normalize();
-	
-	return this;
-}
+    this.numerator = n1 + n2;
+    this.denominator = lcm;
 
+    this.normalize();
+    return this;
+  }
 
-Rational.prototype.add = function(other)
-{
-	this.addInteger(other.integer);
-	this.addFraction(other.numerator, other.denominator);
-	
-	return this;
-}
+  subtract(other) {
+    return this.add(other.clone().negate());
+  }
 
+  multiply(other) {
+    this.numerator *= other.numerator;
+    this.denominator *= other.denominator;
 
-Rational.prototype.subtract = function(other)
-{
-	var commonDenominator = other.denominator * this.denominator;
-	
-	this.numerator = this.getAsNumerator(commonDenominator) - other.getAsNumerator(commonDenominator);
-	this.denominator = commonDenominator;
-	this.integer = 0;
-	
-	this.normalize();
-	
-	return this;
-}
+    this.normalize();
+    return this;
+  }
 
+  negate() {
+    this.numerator *= -1;
+    return this;
+  }
 
-Rational.prototype.subtractFrom = function(other)
-{
-	var commonDenominator = other.denominator * this.denominator;
-	
-	this.numerator = other.getAsNumerator(commonDenominator) - this.getAsNumerator(commonDenominator);
-	this.denominator = commonDenominator;
-	this.integer = 0;
-	
-	this.normalize();
-	
-	return this;
-}
+  compare(other) {
+    const diff =
+      this.numerator * other.denominator -
+      other.numerator * this.denominator;
 
+    return diff < 0 ? -1 : diff > 0 ? 1 : 0;
+  }
 
-Rational.prototype.multiply = function(other)
-{
-	var numerator = this.integer * this.denominator + this.numerator;
-	var otherNumerator = other.integer * other.denominator + other.numerator;
-	
-	this.integer = 0;
-	this.numerator = numerator * otherNumerator;
-	this.denominator = this.denominator * other.denominator;
-	
-	this.normalize();
-	
-	return this;
-}
-
-
-Rational.prototype.addInteger = function(value)
-{
-	this.integer += value;
-	
-	return this;
-}
-
-
-Rational.prototype.addFraction = function(numerator, denominator)
-{
-	if ((denominator % this.denominator) == 0)
-	{
-		var scale = denominator / this.denominator;
-		this.numerator *= scale;
-		this.denominator *= scale;
-	}
-	
-	if (denominator == this.denominator)
-		this.numerator += numerator;
-	
-	else
-	{
-		var commonDenominator = denominator * this.denominator;
-		
-		var scale1 = commonDenominator / this.denominator;
-		var scale2 = commonDenominator / denominator;
-		
-		this.numerator *= scale1;
-		this.numerator += scale2 * numerator;
-		
-		this.denominator = commonDenominator;
-	}
-	
-	this.normalize();
-	
-	return this;
-}
-
-
-Rational.prototype.compare = function(other)
-{
-	if (this.integer != other.integer)
-	{
-		if (this.integer < other.integer)
-			return -1;
-		else
-			return 1;
-	}
-	
-	else
-	{
-		var commonDenominator = other.denominator * this.denominator;
-		
-		var scale1 = commonDenominator / this.denominator;
-		var scale2 = commonDenominator / other.denominator;
-		
-		var numerator1 = this.numerator * scale1;
-		var numerator2 = other.numerator * scale2;
-		
-		if (numerator1 < numerator2)
-			return -1;
-		else if (numerator1 > numerator2)
-			return 1;
-		else
-			return 0;
-	}
+  static fromFloat(value, precision = 1000000) {
+    const numerator = Math.round(value * precision);
+    return new Rational(numerator, precision);
+  }
 }
